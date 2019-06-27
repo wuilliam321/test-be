5da35dde93d53116f019bd61c6f07428afb817c6
Wuilliam Lacruz <wuilliam321@gmail.com>
2019-06-27 12:31:15 -0300

    Adding session model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :searches
   resources :settings
+  resources :sessions
 
   root 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
